d0438fd6f50cc1760aea45f638b1cec59d92fa76
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-08 17:13:03 +0530

    feat : task 2 javascript implemented

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchBar = document.querySelector('#search-bar');
+const clearButton = documnet.querySelector('#clear-btn');
+const rateFilter = document.querySelector('#ratting');
+const countryFilter = document.querySelector('#country');
+
+
