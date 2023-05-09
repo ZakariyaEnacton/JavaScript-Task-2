64a3cde01286a54657e9efbafab7ca67fac2c331
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-09 12:20:07 +0530

    feat : fetch from the API

--- script.js
+++ script.js
@@ -8,35 +8,24 @@ const blogSection = document.querySelector('.blog-section')
 
 const api_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US";
 
-// fetch(api_url)
-//     .then((res) => res.json)
-//     .then((data) => {
-//         var value = data;
-//         console.log(data);
-//     })
-//     .catch()
 
 const getData = async () => {
     const respons = await fetch(api_url)
     const data = await respons.json()
     const result = data.results
-    // console.log(data);
-    console.log(result);
     return result;
 }
-// getData()
-
 
 const displayData = async () => {
     const display = await getData();
 
     let value = display.map((object) => {
-        const { title, vote_average, original_language } = object
-
+        const { title, vote_average, original_language, backdrop_path } = object
+        // console.log(object);
         return `
         <div class="blog-card" id="card">
             <div class="blog-card-image">
-                <img class="card-image" src="images/img3.jpeg" alt="Loding...">
+                <img class="card-image" src="https://image.tmdb.org/t/p/original${backdrop_path}" alt="Loding...">
             </div>
             <div class="card-footer">
                 <div class="title">
@@ -49,12 +38,24 @@ const displayData = async () => {
             </div>
         </div>
         `
-        // console.log(object);
     }).join("")
     blogCard.innerHTML = value
 }
 displayData()
 
-const filterMovieList = () => {
 
-}
+rateFilter.addEventListener('change', async () => {
+    const filter = await getData();
+    filter.filter((r) => {
+        console.log(r.vote_average);
+    })
+})
+
+// rateFilter.addEventListener('change', async () => {
+//     const filter = await getData();
+
+//     filter.filter((rate) => {
+//         console.log(rate.vote_average);
+
+//     })
+// })
