21dd948621d8f21f4b595c5c02d24d1a37cecd57
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-05-09 14:22:01 +0530

    feat : js update for task 2

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const searchBar = document.querySelector('#search-bar');
 const clearButton = document.querySelector('#clear-btn');
 const rateFilter = document.querySelector('#ratting');
-const countryFilter = document.querySelector('#country');
+const langFilter = document.querySelector('#language');
 const cards = document.querySelector('#card')
 const blogCard = document.querySelector('#blog-card')
 const blogSection = document.querySelector('.blog-section')
 
 const api_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US";
 
-
 const getData = async () => {
-    const respons = await fetch(api_url)
+    const respons = await fetch(api_url2)
     const data = await respons.json()
     const result = data.results
     return result;
@@ -28,13 +27,15 @@ const displayData = async () => {
                 <img class="card-image" src="https://image.tmdb.org/t/p/original${backdrop_path}" alt="Loding...">
             </div>
             <div class="card-footer">
-                <div class="title">
-                    <h6 class="movie-name">${title}</h6>
-                </div>
-                <div class="language">Language : ${original_language}</div>
-                <div class="ratting-block">
-                    <p class="movie-ratting">${vote_average}</p>
-                </div>
+            <div class="title">
+                <h6 class="movie-name">${title}</h6>
+            </div>
+            <div class="language">                            
+                <p class="language">Language : ${original_language}</p>
+            </div>
+            <div class="ratting-block">
+                <p class="movie-ratting">${vote_average}</p>
+            </div>
             </div>
         </div>
         `
@@ -46,16 +47,17 @@ displayData()
 
 rateFilter.addEventListener('change', async () => {
     const filter = await getData();
+
     filter.filter((r) => {
-        console.log(r.vote_average);
+        r.vote_average <= rateFilter.value;
     })
 })
 
-// rateFilter.addEventListener('change', async () => {
-//     const filter = await getData();
+langFilter.addEventListener('change', async () => {
+    const lang = await getData()
 
-//     filter.filter((rate) => {
-//         console.log(rate.vote_average);
-
-//     })
-// })
+    lang.filter((l) => {
+        const language = l.original_language == langFilter.value
+        console.log(language);
+    })
+})
